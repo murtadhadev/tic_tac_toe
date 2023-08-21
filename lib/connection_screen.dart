703274bc84ebf93a784926cc03d911dd8c3e6135
mortezaahmed <mortadha.network@gmail.com>
2023-08-21 13:20:29 +0300

    Initial commit

---
+++ lib/connection_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/server_screen.dart';
+
+import 'client_screen.dart';
+
+class ConnectionScreen extends StatefulWidget {
+  const ConnectionScreen({super.key});
+
+  @override
+  State<ConnectionScreen> createState() => _ConnectionScreenState();
+}
+
+class _ConnectionScreenState extends State<ConnectionScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(
+      length: 2,
+      vsync: this,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            TabBar(
+              controller: tabController,
+              tabs: const [
+                Tab(
+                  text: "Connect",
+                ),
+                Tab(
+                  text: "Host",
+                ),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                controller: tabController,
+                children: const [
+                  ClientScreen(),
+                  ServerScreen(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+}
